363c15ee010ddc6a41c2c22eaa88e57360e5a89c
Dassyareg <dassyareg@gmail.com>
2023-01-01 12:53:36 +0100

    set up a modular structure

---
+++ app/app.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"html/template"
+	"net/http"
+
+)
+
+type App struct {
+	Router *Router
+	Server *http.Server
+}
+
+func (app *App) Init(tpl *template.Template) {
+	app.Router = &Router{}
+	app.Router.Init(tpl)
+}
+
+func (app *App) Run() {
+	app.Server = &http.Server{
+		Addr:"8080",
+		Handler: app.Router.Mux,
+	}
+
+	app.Server.ListenAndServe()
+}
